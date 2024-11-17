74916c2bb7ad1daf2845cfbbb6e3e499dea6e116
VARSHINI A <anv.varshini@gmail.com>
2024-11-17 11:19:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add smooth scrolling for navigation
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
